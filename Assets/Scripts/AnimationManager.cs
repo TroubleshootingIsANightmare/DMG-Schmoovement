61f1bb18e48a34381fd924b63bb323e0d7511aec
TroubleshootingIsANightmare <116925522+TroubleshootingIsANightmare@users.noreply.github.com>
2024-04-06 16:39:56 -0500

    YEAH BABY ANIMATION WORKS!
    the running animation animates, this is crazy!

---
+++ Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    public Animator playerAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerAnimator.SetFloat("PosX", Input.GetAxisRaw("Horizontal"));
+        playerAnimator.SetFloat("PosY", Input.GetAxisRaw("Vertical"));
+    }
+}
