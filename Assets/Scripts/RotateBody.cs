6db0d7ee9ec2c581c6c3cb377c68085cddabf82d
TroubleshootingIsANightmare <116925522+TroubleshootingIsANightmare@users.noreply.github.com>
2024-04-14 15:45:31 -0500

    i dont know anymore

--- Assets/Scripts/RotateBody.cs
+++ Assets/Scripts/RotateBody.cs
@@ -7,12 +7,13 @@ using Unity.Mathematics;
 
 public class RotateBody : MonoBehaviour
 {
-    public Transform target, body;
+    public Transform target, body, gun;
     public CameraManager cameraManager;
 
     void LateUpdate()
     {
         GetMiddle();
+        
         RotBody();
     }
 
@@ -33,23 +34,20 @@ public class RotateBody : MonoBehaviour
             targetPoint = ray.origin + (ray.direction * 75f);
             target.position = targetPoint;
         }
+
     }
 
     void RotBody()
     {
         // Get the direction from this object to the target
         Vector3 direction = target.position - body.position;
-
-        // Since the body has a -180x rotation, adjust the forward direction accordingly
-        Vector3 adjustedDirection = Quaternion.Euler(0, 0, -11) * direction;
-
         // Project the direction onto the Y plane (ignore horizontal movement)
-        adjustedDirection.x = 0; adjustedDirection.z = 0;
+        direction.x = 0; direction.z = 0;
 
         // Get the rotation that looks along the direction
-        Quaternion rotation = Quaternion.LookRotation(adjustedDirection);
+        Quaternion rotation = Quaternion.LookRotation(direction);
 
         // Apply the rotation to the body's transform
-        body.localRotation = Quaternion.Euler(-180, 0, -adjustedDirection.y);
+        body.localRotation = Quaternion.Euler(-180, 0, -direction.y);
     }
 }
