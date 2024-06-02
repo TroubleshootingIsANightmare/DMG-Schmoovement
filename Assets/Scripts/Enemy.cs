d5e23f2482d75eaf16c32d2ece49c806b965fbfa
TroubleshootingIsANightmare <116925522+TroubleshootingIsANightmare@users.noreply.github.com>
2024-06-01 19:35:03 -0500

    Enemy Ragdoll

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,27 +8,57 @@ public class Enemy : MonoBehaviour
     public NavMeshAgent agent;
     public Vector3 target;
     public Animator animator;
+    
     public Rigidbody rb;
+    public Collider[] ragdollColliders;
+    public Rigidbody[] ragdollRigidbodies;
+
+    public bool ragdolled = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        ragdollColliders = GetComponentsInChildren<Collider>();
+        ragdollRigidbodies = GetComponentsInChildren<Rigidbody>();
         agent = this.GetComponent<NavMeshAgent>();
         rb = this.GetComponent<Rigidbody>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        agent.enabled = !ragdolled;
+        if (!ragdolled) { DisableNav(); }
+    }
+
+    public void DisableNav()
+    {
         target = GameObject.Find("Player").transform.position;
         agent.SetDestination(target);
-        if(rb.velocity.magnitude > 0f)
+        if (rb.velocity.magnitude > 0f)
         {
             animator.SetBool("Running", true);
-        } else
+        }
+        else
         {
             animator.SetBool("Running", false);
         }
-        this.gameObject.transform.LookAt(target);
+        Vector3 dir = target - gameObject.transform.position;
+        dir.y = 0;
+        Quaternion rot = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Lerp(gameObject.transform.rotation, rot, 3f * Time.deltaTime);
+    }
+
+    public void EnableRagdoll(bool enabled)
+    {
+        animator.enabled = !enabled;
+        foreach(Rigidbody item in ragdollRigidbodies)
+        {
+            item.useGravity = enabled;
+            item.isKinematic = !enabled;
+        }
+        rb.useGravity = !enabled;
+        rb.isKinematic = enabled;
+        ragdolled = enabled;
     }
 }
--- Assets/Scripts/ExplosiveProjectile.cs
+++ Assets/Scripts/ExplosiveProjectile.cs
@@ -11,7 +11,9 @@ public class ExplosiveProjectile : MonoBehaviour
     public float radius = 6.5f;
     public GameObject explosiveFX;
     private GameObject projectile;
-    
+    public Enemy enemy;
+    public LayerMask canExplode;
+    public int exploded = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,33 +22,44 @@ public class ExplosiveProjectile : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-
-            if (other.gameObject.tag == "Ground")
+            if ((other.gameObject.tag == "Ground" || other.gameObject.tag == "Enemy" ) && exploded < 2)
             {
+                exploded++;
+                Destroy(projectile);
                 Explode();
+            if (other.gameObject.tag == "Enemy") enemy.EnableRagdoll(true);
             }
        
     }
 
+    private void Update()
+    {
+        enemy = GameObject.Find("Enemy").GetComponent<Enemy>();
+    }
+
     void Explode()
     {
+        Debug.Log("AOKSJDAJKSDASKLDJ");
         Vector3 explosionPos = transform.position;
-        Collider[] hits = Physics.OverlapSphere(explosionPos, radius);
+        Collider[] hits = Physics.OverlapSphere(explosionPos, radius, canExplode.value);
         Instantiate(explosiveFX, projectile.transform.position, projectile.transform.rotation);
+
+        HashSet<Rigidbody> affectedRigidbodies = new HashSet<Rigidbody>();
         foreach (Collider hit in hits)
         {
             Rigidbody rb = hit.GetComponent<Rigidbody>();
 
-
-            if (rb != null)
+            if (rb != null && !affectedRigidbodies.Contains(rb))
             {
+                if (rb.gameObject.tag == "Enemy" && !enemy.ragdolled) enemy.EnableRagdoll(true);
                 Vector2 locate = new Vector2(hit.gameObject.transform.position.x, hit.gameObject.transform.position.z);
                 Vector2 direction = new Vector2(locate.x - explosionPos.x, locate.y - explosionPos.z);
                 direction.Normalize();
-                Destroy(projectile);
                 rb.AddForce(new Vector3(direction.x * knockback, 0, direction.y * knockback));
                 rb.AddExplosionForce(power, explosionPos, radius, 4);
+                affectedRigidbodies.Add(rb);
             }
+
         }
     }
 
