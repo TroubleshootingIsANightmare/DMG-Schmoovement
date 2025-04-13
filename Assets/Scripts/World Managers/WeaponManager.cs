0437e8becd4253b8a52298897edb17cc538b3a30
TroubleshootingIsANightmare <116925522+TroubleshootingIsANightmare@users.noreply.github.com>
2025-04-12 20:23:39 -0500

    Added water gun model
    Still not finished with my additions

--- Assets/Scripts/World Managers/WeaponManager.cs
+++ Assets/Scripts/World Managers/WeaponManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    public GameObject[] weapons;
+    public int currentWeapon = 0;
+    private int nrWeapons;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nrWeapons = weapons.Length;
+
+        SwitchWeapon(currentWeapon); // Set default gun
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        for (int i = 1; i <= nrWeapons; i++)
+        {
+            if (Input.GetKeyDown("" + i))
+            {
+                currentWeapon = i - 1;
+
+                SwitchWeapon(currentWeapon);
+
+            }
+        }
+    }
+
+    void SwitchWeapon(int index)
+    {
+        for (int i = 0; i < nrWeapons; i++)
+        {
+            if (i == index)
+            {
+                weapons[index].SetActive(true);
+            }
+            else
+            {
+                weapons[i].SetActive(false);
+            }
+        }
     }
 }
